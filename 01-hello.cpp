39e2da94dec3677ab4383464c2cc15c53d5fa7ee
HillZWEI <123815906+hillzwei@users.noreply.github.com>
2023-01-31 11:56:55 +0000

    01-hello

---
+++ 01-hello.cpp
@@ -0,0 +1,11 @@
+// Standard library uses <xxx> while user library uses "xxx"
+#include<iostream>
+
+// When not use namespace std, cout will be write as std::cout
+using namespace std;
+
+int main() {
+    // Output to standard output.
+    cout << "hello, it works" << endl;
+    return 0;
+}
